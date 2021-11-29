838ae5b1ef3926b6fc936dd73e6e56356f81eb15
Michael van der Haas <kiwi3d@gmail.com>
2021-11-30 07:09:17 +1300

    init

--- keystone.ts
+++ keystone.ts
@@ -14,13 +14,16 @@ import { lists } from './schema';
 // Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
 import { withAuth, session } from './auth';
 
+
+require('dotenv').config();
+
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'postgresql',
-      url: 'postgres://bkfnogakjinwkv:eb7133e27d43ed60c82eae8e590d5c644282509974810e9d354b1bd1396b0fd0@ec2-54-237-183-160.compute-1.amazonaws.com:5432/d6a1j59majse',
+      provider: process.env.DATABASE_PROVIDER,
+      url: process.env.DATABASE_URL
     },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
